8a7deafd2e36804bb4177cfe564485e7ab5246f7
britobarbara <babi.bolacha@gmail.com>
2024-06-18 13:39:45 -0300

    criando banco com java

---
+++ src/Banco.java
@@ -0,0 +1,17 @@
+import java.util.List;
+
+public class Banco {
+    private String nome;
+    private List<Conta> contas;
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+    public Banco(List<Conta> contas) {
+        this.contas = contas;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+     Cliente barbara = new Cliente();
+     barbara.setNome("Barbara");
+
+     Conta cc = new ContaCorrente(barbara);
+     Conta poupanca = new ContaPoupanca(barbara);
+
+     cc.depositar(100);
+     cc.transferir(100, poupanca);
+
+     cc.imprimirExtrato();
+     poupanca.imprimirExtrato();
+
+    }
+}
